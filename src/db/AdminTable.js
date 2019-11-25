ea01b8fbade76fee828db434381d9921621c2090
ThiagoViBritannia <41394481+ThiagoViBritannia@users.noreply.github.com>
2019-11-25 15:45:20 -0400

    Update AdminTable.js

--- src/db/AdminTable.js
+++ src/db/AdminTable.js
@@ -1,37 +1,16 @@
 var tbAdmin = [
     {
-        nome: "Micardo Rilos",
-        area: "Pimbada Distribuida",
+        nome: "Ricardo",
+        area: "Informática",
         cpf: "666.666.666.69",
         ra: "692451",
         senha: 0x123cdf21341234,
     },
     {
-        nome: "Sirel Mina",
-        area: "Aspiração Empreendedora",
+        nome: "Mirel",
+        area: "Informática",
         cpf: "666.666.667.69",
         ra: "692451",
-        senha: 0x123cdf21341234,
-    },
-    {
-        nome: "Mlauco Gaya",
-        area: "Explosão Anti-Biba",
-        cpf: "666.666.668.69",
-        ra: "692451",
-        senha: 0x123cdf21341234,
-    },
-    {
-        nome: "Cearense",
-        area: "Cabeçada de 3 minutos",
-        cpf: "666.666.669.69",
-        ra: "692451",
-        senha: 0x123cdf21341234,
-    },
-    {
-        nome: "Ubuntu",
-        area: "Zueira Unlimited LTDA",
-        cpf: "Maq",
-        ra: "11",
         senha: 0x123cdf21341234,
     },
 ]
