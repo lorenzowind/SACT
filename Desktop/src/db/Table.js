25c947fe037a1850038b919390bd04650043a2ab
Lorenzo Windmoller <lorenzomart01@gmail.com>
2020-04-02 23:48:11 -0400

    Web system environment created and API organized in a shared location

--- Desktop/src/db/Table.js
+++
@@ -1,7 +0,0 @@
-import * as proj from "./ProjectTable";
-import * as aval from "./AvaliatorTable";
-import * as admin from "./AdminTable";
-
-export const project = proj;
-export const avaliator = aval;
-export const administrator = admin;
