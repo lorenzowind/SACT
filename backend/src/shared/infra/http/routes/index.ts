ae23a2a7fe96cce309ee47a5fde5d2a2d3002a18
lorenzowind <lorenzomart01@gmail.com>
2020-08-27 10:34:37 -0400

    fix: routes reference

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
-// import usersRouter from '@modules/users/infra/http/routes/users.routes';
-// import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
-// import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import adminsRouter from '@modules/admins/infra/http/routes/admins.routes';
+import sessionsRouter from '@modules/admins/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/admins/infra/http/routes/password.routes';
 
 const routes = Router();
 
-// routes.use('/users', usersRouter);
-// routes.use('/sessions', sessionsRouter);
-// routes.use('/password', passwordRouter);
+routes.use('/admins', adminsRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
