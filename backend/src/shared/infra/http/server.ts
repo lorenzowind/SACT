97f7f8596c1cb96990ea11ef4d614817915fdd49
lorenzowind <lorenzomart01@gmail.com>
2020-08-27 08:32:25 -0400

    chore: docker configuration and application dependences

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
+import cors from 'cors';
+import 'express-async-errors';
+
+import '../typeorm/database';
+// import '@shared/container/index';
+
+import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
+import AppError from '../../errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(rateLimiter);
+app.use(routes);
+
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333!');
+});
