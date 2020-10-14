5ea4b5f797440befd910da38b977dc998f3d6d20
lorenzowind <lorenzomart01@gmail.com>
2020-10-14 16:51:09 -0400

    feat: functional evaluators listing

--- frontend/src/pages/Admin/Evaluators/index.tsx
+++ frontend/src/pages/Admin/Evaluators/index.tsx
@@ -1,56 +1,156 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { IoIosAddCircleOutline } from 'react-icons/io';
 
-import HeaderAdm from '../../../components/Header';
+import { FiEdit2, FiSearch } from 'react-icons/fi';
+import api from '../../../services/api';
 
-import BackImg from '../../../assets/icon_back.png';
-import LupaImg from '../../../assets/icon_search.png';
+import { useToast } from '../../../hooks/toast';
 
-import { Background, Container, Content, EvaluatorContainer } from './styles';
+import {
+  Background,
+  Container,
+  SecondaryHeader,
+  TableContainer,
+} from './styles';
+
+import Header from '../../../components/Header';
+import Loading from '../../../components/Loading';
+
+interface EvaluatorData {
+  id: string;
+  name: string;
+  occupation_area: string;
+  institution: string;
+  phone_number: string;
+  email: string;
+  status: string;
+}
 
 const Evaluators: React.FC = () => {
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+
+  const [evaluatorSearch, setEvaluatorSearch] = useState('');
+  const [evaluators, setEvaluators] = useState<EvaluatorData[]>([]);
+
+  const { addToast } = useToast();
+
+  const handleSearch = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      await api
+        .get<EvaluatorData[]>(`evaluators/all?search=${evaluatorSearch}`)
+        .then(response => {
+          setEvaluators(response.data);
+
+          if (!response.data.length) {
+            addToast({
+              type: 'info',
+              title: 'Nenhum avaliador encontrado',
+            });
+          }
+        });
+    } catch (err) {
+      addToast({
+        type: 'error',
+        title: 'Erro na busca por avaliadores',
+      });
+    } finally {
+      setLoading(false);
+    }
+  }, [addToast, evaluatorSearch]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        setLoading(true);
+
+        await api.get<EvaluatorData[]>('evaluators/all').then(response => {
+          setEvaluators(response.data);
+        });
+      } catch (err) {
+        addToast({
+          type: 'error',
+          title: 'Erro na busca por avaliadores',
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadData();
+  }, [addToast]);
+
   return (
-    <Background>
-      <Container>
-        {' '}
-        <HeaderAdm />
-        <Content>
-          <Link
-            to="/dashboard"
-            style={{
-              gridArea: 'back',
-              height: 71,
+    <>
+      {loading && <Loading zIndex={1} />}
 
-              cursor: 'pointer',
-              marginTop: 10,
-            }}
-          >
-            <img src={BackImg} alt="Voltar" height={71} />
+      <Header />
+
+      <Background>
+        <SecondaryHeader>
+          <Link to="dashboard">
+            <FontAwesomeIcon size="3x" icon={faChevronLeft} />
           </Link>
+          <strong>Avaliadores</strong>
+        </SecondaryHeader>
 
-          <EvaluatorContainer>
-            <h1>Avaliadores</h1>
-            <h2>Pesquisar Avaliadores</h2>
-            <div className="input-text">
-              <input type="text" />
-              <img
-                src={LupaImg}
-                alt="Pesquisar"
-                height={25}
-                style={{ justifySelf: 'end', paddingRight: '5px' }}
+        <Container>
+          <form>
+            <strong>Pesquisar avaliadores</strong>
+            <div>
+              <input
+                type="text"
+                placeholder="Nome"
+                value={evaluatorSearch}
+                onChange={e => setEvaluatorSearch(e.target.value)}
               />
+              <button type="button" onClick={handleSearch}>
+                <FiSearch />
+              </button>
             </div>
-            <div className="add-eva">
-              {' '}
-              <h2>Adicionar Avaliadores</h2>
-              <Link to="/evaluator-form" style={{ textDecoration: 'none' }}>
-                <div className="btn-add">+</div>
-              </Link>
-            </div>
-          </EvaluatorContainer>
-        </Content>
-      </Container>
-    </Background>
+          </form>
+          <div>
+            <strong>Adicionar avaliador</strong>
+
+            <button type="button">
+              <IoIosAddCircleOutline />
+            </button>
+          </div>
+          <TableContainer>
+            <thead>
+              <tr>
+                <th>Avaliador</th>
+                <th>Área de atuação</th>
+                <th>Telefone</th>
+                <th>Email</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {evaluators.map(evaluator => (
+                <tr key={evaluator.id}>
+                  <td>{evaluator.name}</td>
+                  <td>{evaluator.occupation_area}</td>
+                  <td>{evaluator.phone_number}</td>
+                  <td>{evaluator.email}</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit2 />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </TableContainer>
+        </Container>
+      </Background>
+    </>
   );
 };
 
