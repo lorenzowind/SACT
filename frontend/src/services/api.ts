d9fd5a2f32bd911780bc059e3a6ac50e3e786b9e
lorenzowind <lorenzomart01@gmail.com>
2020-10-15 22:01:44 -0400

    refactor: env configuration

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-18-219-59-120.us-east-2.compute.amazonaws.com/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
