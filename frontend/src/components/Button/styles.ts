2e6928e786ec1252e26ea1a0692aff5d0cde568a
lorenzowind <lorenzomart01@gmail.com>
2020-10-21 01:22:52 -0400

    fix: some conflict errors

--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -1,15 +1,6 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-<<<<<<< HEAD
-export const Main = styled.main`
-  padding-top: 10vh;
-  text-align: center;
-
-  & > input[type='text'] {
-    margin-top: 1rem;
-    margin-bottom: 6rem;
-=======
 export const Container = styled.button`
   border-radius: 10px;
   box-shadow: 2px 2px 6px 0 rgba(0, 0, 0, 0.16);
@@ -27,6 +18,5 @@ export const Container = styled.button`
 
   &:hover {
     background: ${shade(0.2, '#0000fb')};
->>>>>>> master-dev
   }
 `;
